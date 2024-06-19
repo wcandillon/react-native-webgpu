a69ad23f01e6f3cce35c91a0a7696252cadc870b
William Candillon <wcandillon@gmail.com>
2024-06-19 11:13:37 +0200

    :wrench:

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: ["module:@react-native/babel-preset"],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: ["transform-inline-environment-variables"],
 };
