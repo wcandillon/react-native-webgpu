8747fac2677655c43cbacfd8ca7f535bcd0a84db
William Candillon <wcandillon@gmail.com>
2024-06-21 08:38:04 +0200

    :green_heart:

--- package/android/cpp/cpp-adapter.cpp
+++ package/android/cpp/cpp-adapter.cpp
@@ -1,12 +1,12 @@
-#include <fbjni/fbjni.h>
-#include <jni.h>
 #include "RNFLogger.h"
 #include "RNWebGPUManager.h"
+#include <fbjni/fbjni.h>
+#include <jni.h>
 
 #define LOG_TAG "WebGPUModule"
 
-extern "C" JNIEXPORT void JNICALL
-Java_com_webgpu_WebGPUModule_initializeNative(JNIEnv *env, jobject /* this */, jlong jsRuntime, jobject jsInvokerHolder) {
-    auto runtime = reinterpret_cast<facebook::jsi::Runtime*>(jsRuntime);
-    auto manager = new rnwgpu::RNWebGPUManager(runtime, nullptr);
+extern "C" JNIEXPORT void JNICALL Java_com_webgpu_WebGPUModule_initializeNative(
+    JNIEnv *env, jobject /* this */, jlong jsRuntime, jobject jsInvokerHolder) {
+  auto runtime = reinterpret_cast<facebook::jsi::Runtime *>(jsRuntime);
+  auto manager = new rnwgpu::RNWebGPUManager(runtime, nullptr);
 }
