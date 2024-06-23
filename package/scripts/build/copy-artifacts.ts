45748af27c0aa92f8a4dcc4c2f5f2c220a2eff4e
William Candillon <wcandillon@gmail.com>
2024-06-23 14:56:19 +0200

    :wrench:

---
+++ package/scripts/build/copy-artifacts.ts
@@ -0,0 +1,12 @@
+import { $, checkFileExists } from "./util";
+
+$("cp -R ../artifacts/libs .");
+$("cp -R ../artifacts/cpp/dawn cpp");
+$("cp -R ../artifacts/cpp/webgpu cpp");
+checkFileExists("libs/android/x86/libwebgpu_dawn.a");
+checkFileExists("libs/android/x86_64/libwebgpu_dawn.a");
+checkFileExists("libs/android/armeabi-v7a/libwebgpu_dawn.a");
+checkFileExists("libs/android/arm64-v8a/libwebgpu_dawn.a");
+checkFileExists("libs/ios/libwebgpu_dawn.xcframework");
+checkFileExists("libs/ios/arm64_iphoneos/libwebgpu_dawn.a");
+checkFileExists("cpp/dawn/webgpu_cpp.h");
---
+++ package/scripts/build/dawn.ts
@@ -0,0 +1,102 @@
+/* eslint-disable camelcase */
+/* eslint-disable max-len */
+import type { Platform, SDK } from "./util";
+import { $, build, copyLib, mapKeys } from "./util";
+
+const PATH = `PATH=${__dirname}/../../../externals/depot_tools/:$PATH`;
+
+const commonArgs = {
+  CMAKE_BUILD_TYPE: "Release",
+  DAWN_BUILD_SAMPLES: "OFF",
+  TINT_BUILD_TESTS: "OFF",
+  TINT_BUILD_CMD_TOOLS: "OFF",
+};
+
+const android = {
+  platforms: ["arm64-v8a", "armeabi-v7a", "x86", "x86_64"] as Platform[],
+  args: {
+    CMAKE_TOOLCHAIN_FILE: "$ANDROID_NDK/build/cmake/android.toolchain.cmake",
+    ANDROID_PLATFORM: "android-26",
+    ...commonArgs,
+  },
+};
+
+type BuildMatrix = Record<Platform, SDK[]>;
+
+const ios = {
+  matrix: {
+    arm64: ["iphoneos", "iphonesimulator"],
+    x86_64: ["iphonesimulator"],
+  } as BuildMatrix,
+  args: {
+    CMAKE_SYSTEM_NAME: "iOS",
+    CMAKE_OSX_DEPLOYMENT_TARGET: "13.0",
+    DAWN_USE_GLFW: "OFF",
+    BUILD_SAMPLES: "OFF",
+    ...commonArgs,
+  },
+};
+
+(async () => {
+  // Fetch dependencies
+  console.log("gclient sync");
+  process.chdir("../externals/dawn");
+  $("cp scripts/standalone.gclient .gclient");
+  $(`${PATH} gclient sync`);
+  process.chdir("../..");
+  console.log("gclient sync done");
+
+  // Build iOS
+  for (const platform of mapKeys(ios.matrix)) {
+    console.log(`Build iOS: ${platform}`);
+    for (const sdk of ios.matrix[platform]) {
+      await build(`ios_${platform}_${sdk}`, {
+        CMAKE_OSX_ARCHITECTURES: platform,
+        CMAKE_OSX_SYSROOT: `$(xcrun --sdk ${sdk} --show-sdk-path)`,
+        ...ios.args,
+      });
+      copyLib("ios", platform, sdk);
+    }
+  }
+
+  console.log("Building fat binary for iphone simulator");
+  const libs = [
+    "libwebgpu_dawn",
+    "libdawn_native",
+    "libdawn_proc",
+    "libdawn_common",
+  ];
+  libs.forEach((lib) => {
+    $(
+      `lipo -create package/libs/ios/x86_64_iphonesimulator/${lib}.a package/libs/ios/arm64_iphonesimulator/${lib}.a -output package/libs/ios/${lib}.a`,
+    );
+  });
+
+  libs.forEach((lib) => {
+    console.log("Building libwebgpu_dawn.xcframework");
+    $("rm -rf ./package/libs/ios/libwebgpu_dawn.xcframework");
+    $(
+      "xcodebuild -create-xcframework " +
+        `-library ./package/libs/ios/${lib}.a ` +
+        `-library ./package/libs/ios/arm64_iphoneos/${lib}.a ` +
+        ` -output ./package/libs/ios/${lib}.xcframework `,
+    );
+  });
+  // Build Android
+  for (const platform of android.platforms) {
+    console.log(`Build Android: ${platform}`);
+    await build(`android_${platform}`, {
+      ANDROID_ABI: platform,
+      ...android.args,
+    });
+    copyLib("android", platform);
+  }
+
+  console.log("Copy headers");
+  $("cp -R externals/dawn/out/android_arm64-v8a/gen/include/* package/cpp");
+  $("cp externals/dawn/include/webgpu/webgpu_cpp.h package/cpp/webgpu/");
+  $(
+    "cp externals/dawn/include/webgpu/webgpu_enum_class_bitmasks.h package/cpp/webgpu/",
+  );
+  $("cp externals/dawn/include/webgpu/webgpu.h package/cpp/webgpu/");
+})();
