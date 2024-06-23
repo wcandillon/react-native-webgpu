4320dc2a32a72f20315c7756bcbdbd89a1774420
William Candillon <wcandillon@gmail.com>
2024-06-23 17:03:56 +0200

    💚 (#8)

--- package/scripts/build/copy-artifacts.ts
+++ package/scripts/build/copy-artifacts.ts
@@ -1,12 +1,6 @@
-import { $, checkFileExists } from "./util";
+import { $, checkBuildArtifacts } from "./util";
 
 $("cp -R ../artifacts/libs .");
 $("cp -R ../artifacts/cpp/dawn cpp");
 $("cp -R ../artifacts/cpp/webgpu cpp");
-checkFileExists("libs/android/x86/libwebgpu_dawn.a");
-checkFileExists("libs/android/x86_64/libwebgpu_dawn.a");
-checkFileExists("libs/android/armeabi-v7a/libwebgpu_dawn.a");
-checkFileExists("libs/android/arm64-v8a/libwebgpu_dawn.a");
-checkFileExists("libs/ios/libwebgpu_dawn.xcframework");
-checkFileExists("libs/ios/arm64_iphoneos/libwebgpu_dawn.a");
-checkFileExists("cpp/dawn/webgpu_cpp.h");
+checkBuildArtifacts();
