13e74a3c7493e8479d488bdd4b24aee931581eea
William Candillon <wcandillon@gmail.com>
2024-06-23 18:09:12 +0200

    :wrench:

--- package/scripts/codegen/bindings.ts
+++ package/scripts/codegen/bindings.ts
@@ -1,30 +1,31 @@
-import * as fs from "fs";
 import * as path from "path";
 
-import * as ts from "@typescript-eslint/typescript-estree";
+import { Project } from "ts-morph";
 
 // Define the path to the WebGPU type declaration file
+const tsConfigFilePath = path.resolve(__dirname, "../../tsconfig.json");
 const filePath = path.resolve(
   __dirname,
   "../../node_modules/@webgpu/types/dist/index.d.ts",
 );
-
-// Read the content of the file
-const fileContent = fs.readFileSync(filePath, "utf-8");
-
-// Parse the content to an AST
-const ast = ts.parse(fileContent, {
-  loc: true,
-  range: true,
-  tokens: true,
-  comment: true,
-  jsx: false,
-  useJSXTextNode: false,
-  loggerFn: false,
-  errorOnUnknownASTType: false,
-  errorOnTypeScriptSyntacticAndSemanticIssues: false,
-  preserveNodeMaps: true,
+const project = new Project({
+  tsConfigFilePath,
 });
 
-// Log the AST to the console
-console.log(JSON.stringify(ast, null, 2));
+// Add source files to the project
+const sourceFile = project.addSourceFileAtPath(filePath);
+
+// Inspecting the AST
+sourceFile.forEachChild((node) => {
+  console.log(`Node kind: ${node.getKindName()}`);
+  console.log(`Node text: ${node.getText()}`);
+});
+
+// Alternatively, you can navigate through the nodes
+const classes = sourceFile.getClasses();
+classes.forEach((cls) => {
+  console.log(`Class name: ${cls.getName()}`);
+  cls.getMethods().forEach((method) => {
+    console.log(`Method name: ${method.getName()}`);
+  });
+});
