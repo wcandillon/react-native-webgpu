129fb57698e4642ac38eb81378290cb2a5c4fca2
William Candillon <wcandillon@gmail.com>
2024-07-03 20:57:44 +0200

    :wrench:

--- package/example/src/Tests.tsx
+++ package/example/src/Tests.tsx
@@ -1,6 +1,45 @@
-import React from "react";
-import { View } from "react-native";
+/* eslint-disable no-eval */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect } from "react";
+import { Text, View } from "react-native";
+
+import { useClient } from "./useClient";
+
+export const CI = process.env.CI === "true";
 
 export const Tests = () => {
-  return <View />;
+  const [client, hostname] = useClient();
+  useEffect(() => {
+    if (client !== null) {
+      client.onmessage = (e) => {
+        const tree: any = JSON.parse(e.data);
+        if (tree.code) {
+          client.send(
+            JSON.stringify(
+              eval(
+                `(function Main() {
+                  return (${tree.code})(this.ctx);
+                })`,
+              ).call({
+                ctx: JSON.parse(tree.ctx),
+              }),
+            ),
+          );
+        }
+      };
+      return () => {
+        client.close();
+      };
+    }
+    return;
+  }, [client]);
+  return (
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <Text style={{ color: "black" }}>
+        {client === null
+          ? `⚪️ Connecting to ${hostname}. Use yarn e2e to run tests.`
+          : "🟢 Waiting for the server to send tests"}
+      </Text>
+    </View>
+  );
 };
