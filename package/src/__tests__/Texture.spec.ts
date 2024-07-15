1a64c78e2bd6557de914bc02fe86d7ebe135a2b9
William Candillon <wcandillon@gmail.com>
2024-07-15 20:30:58 +0200

    :wrench:

--- package/src/__tests__/Texture.spec.ts
+++ package/src/__tests__/Texture.spec.ts
@@ -9,18 +9,18 @@ describe("Texture", () => {
     });
     expect(result).toBe(20);
   });
-  // it("create texture", async () => {
-  //   const result = await client.eval(({ device }) => {
-  //     const textureSize = 512;
-  //     const texture = device.createTexture({
-  //       size: [textureSize, textureSize],
-  //       format: "rgba8unorm",
-  //       usage:
-  //         GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.TEXTURE_BINDING,
-  //       label: "texture",
-  //     });
-  //     return texture.label;
-  //   });
-  //   expect(result).toBe("texture");
-  // });
+  it("create texture", async () => {
+    const result = await client.eval(({ device }) => {
+      const textureSize = 512;
+      const texture = device.createTexture({
+        size: [textureSize, textureSize],
+        format: "rgba8unorm",
+        usage:
+          GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.TEXTURE_BINDING,
+        label: "texture",
+      });
+      return texture.label;
+    });
+    expect(result).toBe("texture");
+  });
 });
