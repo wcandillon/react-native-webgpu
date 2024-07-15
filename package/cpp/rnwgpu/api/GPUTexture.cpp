07456737de33ce6dcc1284d59153c11d9a31c391
William Candillon <wcandillon@gmail.com>
2024-07-15 22:18:59 +0200

    :wrench:

--- package/cpp/rnwgpu/api/GPUTexture.cpp
+++ package/cpp/rnwgpu/api/GPUTexture.cpp
@@ -4,4 +4,36 @@ namespace rnwgpu {
 
 void GPUTexture::destroy() { _instance.Destroy(); }
 
+uint32_t GPUTexture::getWidth() {
+  return _instance.GetWidth();
+}
+
+uint32_t GPUTexture::getHeight() {
+  return _instance.GetHeight();
+}
+
+uint32_t GPUTexture::getDepthOrArrayLayers() {
+  return _instance.GetDepthOrArrayLayers();
+}
+
+uint32_t GPUTexture::getMipLevelCount() {
+  return _instance.GetMipLevelCount();
+}
+
+uint32_t GPUTexture::getSampleCount() {
+  return _instance.GetSampleCount();
+}
+
+wgpu::TextureDimension GPUTexture::getDimension() {
+  return _instance.GetDimension();
+}
+
+wgpu::TextureFormat GPUTexture::getFormat() {
+  return _instance.GetFormat();
+}
+
+wgpu::TextureUsage GPUTexture::getUsage() {
+  return _instance.GetUsage()
+}
+
 } // namespace rnwgpu
