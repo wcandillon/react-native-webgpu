e709f8dd2c0f61c74b71fd886fcc5588f0f6c2fc
William Candillon <wcandillon@gmail.com>
2024-07-16 17:45:30 +0200

    :wrench:

--- package/src/__tests__/Shaders.spec.ts
+++ package/src/__tests__/Shaders.spec.ts
@@ -11,48 +11,36 @@ describe("Triangle", () => {
     });
     expect(result).toBe("triangleVertWGSL");
   });
-  // it("creates a shader module", async () => {
-  //   const result = await client.eval(({ device, triangleVertWGSL }) => {
-  //     const mod = device.createShaderModule({
-  //       code: triangleVertWGSL,
-  //       label: "triangleVertWGSL",
-  //     });
-  //     //const { label } = mod;
-  //     return mod.getCompilationInfo().then((info) => JSON.stringify(info));
-  //   });
-  //   console.log({ result });
-  //   expect(true).toBe(true);
-  // });
-  // it("create the pipeline", async () => {
-  //   const result = await client.eval(
-  //     ({ device, triangleVertWGSL, redFragWGSL }) => {
-  //       const pipeline = device.createRenderPipeline({
-  //         layout: "auto",
-  //         vertex: {
-  //           module: device.createShaderModule({
-  //             code: triangleVertWGSL,
-  //           }),
-  //         },
-  //         fragment: {
-  //           module: device.createShaderModule({
-  //             code: redFragWGSL,
-  //           }),
-  //           targets: [
-  //             {
-  //               format: "rgba8unorm",
-  //             },
-  //           ],
-  //         },
-  //         primitive: {
-  //           topology: "triangle-list",
-  //         },
-  //         label: "trianglePipeline",
-  //       });
-  //       return pipeline.label;
-  //     },
-  //   );
-  //   expect(result).toBe("trianglePipeline");
-  // });
+  it("create the pipeline", async () => {
+    const result = await client.eval(
+      ({ device, triangleVertWGSL, redFragWGSL }) => {
+        const pipeline = device.createRenderPipeline({
+          layout: "auto",
+          vertex: {
+            module: device.createShaderModule({
+              code: triangleVertWGSL,
+            }),
+          },
+          fragment: {
+            module: device.createShaderModule({
+              code: redFragWGSL,
+            }),
+            targets: [
+              {
+                format: "rgba8unorm",
+              },
+            ],
+          },
+          primitive: {
+            topology: "triangle-list",
+          },
+          label: "trianglePipeline",
+        });
+        return pipeline.label;
+      },
+    );
+    expect(result).toBe("trianglePipeline");
+  });
   // it("create the pipeline and sample it", async () => {
   //   const result = await client.eval(
   //     ({ device, triangleVertWGSL, redFragWGSL, gpu }) => {
