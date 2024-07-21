207c136149a629bba136f2ba59c36ebfebce6f3a
William Candillon <wcandillon@gmail.com>
2024-07-21 17:59:00 +0200

    :wrench:

--- package/cpp/rnwgpu/api/GPURenderPassEncoder.cpp
+++ package/cpp/rnwgpu/api/GPURenderPassEncoder.cpp
@@ -18,14 +18,26 @@ void GPURenderPassEncoder::draw(uint32_t vertexCount,
 }
 
 void GPURenderPassEncoder::setBindGroup(
-    uint32_t groupIndex, std::shared_ptr<GPUBindGroup> group,
+    uint32_t groupIndex,
+    std::variant<std::nullptr_t, std::shared_ptr<GPUBindGroup>> bindGroup,
     std::optional<std::vector<uint32_t>> dynamicOffsets) {
   auto dynOffsets = dynamicOffsets.value_or(std::vector<uint32_t>());
   if (dynOffsets.size() == 0) {
-    _instance.SetBindGroup(groupIndex, group->get(), 0, nullptr);
+    if (std::holds_alternative<nullptr_t>(bindGroup)) {
+      _instance.SetBindGroup(groupIndex, nullptr, 0, nullptr);
+    } else {
+      auto group = std::get<std::shared_ptr<GPUBindGroup>>(bindGroup);
+      _instance.SetBindGroup(groupIndex, group->get(), 0, nullptr);
+    }
   } else {
-    _instance.SetBindGroup(groupIndex, group->get(), dynOffsets.size(),
-                           dynamicOffsets->data());
+    if (std::holds_alternative<nullptr_t>(bindGroup)) {
+      _instance.SetBindGroup(groupIndex, nullptr, dynOffsets.size(),
+                             dynamicOffsets->data());
+    } else {
+      auto group = std::get<std::shared_ptr<GPUBindGroup>>(bindGroup);
+      _instance.SetBindGroup(groupIndex, group->get(), dynOffsets.size(),
+                             dynamicOffsets->data());
+    }
   }
 }
 
