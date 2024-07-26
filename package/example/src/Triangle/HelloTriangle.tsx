f0e53766c3308e146a39d0541282fc273fdb14d7
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2024-07-26 14:48:58 +0200

    Lint

--- package/example/src/Triangle/HelloTriangle.tsx
+++ package/example/src/Triangle/HelloTriangle.tsx
@@ -43,7 +43,7 @@ export function HelloTriangle() {
         entryPoint: "main",
         targets: [
           {
-            format: presentationFormat
+            format: presentationFormat,
           },
         ],
       },
