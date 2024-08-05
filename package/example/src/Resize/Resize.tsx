3ab606005732fe52a675321cb33b0e5b5f09c94d
William Candillon <wcandillon@gmail.com>
2024-08-05 21:39:21 +0200

    Update example

--- package/example/src/Resize/Resize.tsx
+++ package/example/src/Resize/Resize.tsx
@@ -8,68 +8,97 @@ import { useWebGPU } from "../components/useWebGPU";
 const window = Dimensions.get("window");
 
 export const Resize = () => {
-  const width = useRef(new Animated.Value(window.width));
-  const { canvasRef } = useWebGPU(({ context, device, presentationFormat }) => {
-    const pipeline = device.createRenderPipeline({
-      layout: "auto",
-      vertex: {
-        module: device.createShaderModule({
-          code: triangleVertWGSL,
-        }),
-        entryPoint: "main",
-      },
-      fragment: {
-        module: device.createShaderModule({
-          code: redFragWGSL,
-        }),
-        entryPoint: "main",
-        targets: [
-          {
+  const width = useRef(new Animated.Value(20));
+  const { canvasRef } = useWebGPU(
+    ({ context, device, presentationFormat, canvas }) => {
+      const sampleCount = 4;
+      const pipeline = device.createRenderPipeline({
+        layout: "auto",
+        vertex: {
+          module: device.createShaderModule({
+            code: triangleVertWGSL,
+          }),
+        },
+        fragment: {
+          module: device.createShaderModule({
+            code: redFragWGSL,
+          }),
+          targets: [
+            {
+              format: presentationFormat,
+            },
+          ],
+        },
+        primitive: {
+          topology: "triangle-list",
+        },
+        multisample: {
+          count: sampleCount,
+        },
+      });
+      let currentSize = { width: 0, height: 0 };
+      let renderTarget: GPUTexture | undefined;
+      let renderTargetView: GPUTextureView;
+
+      return () => {
+        if (
+          currentSize.width !== canvas.width ||
+          currentSize.height !== canvas.height
+        ) {
+          if (renderTarget !== undefined) {
+            // Destroy the previous render target
+            renderTarget.destroy();
+          }
+
+          // Setting the canvas width and height will automatically resize the textures returned
+          // when calling getCurrentTexture() on the context.
+          currentSize = { width: canvas.width, height: canvas.height };
+          renderTarget = device.createTexture({
+            size: [canvas.width, canvas.height],
+            sampleCount,
             format: presentationFormat,
-          },
-        ],
-      },
-      primitive: {
-        topology: "triangle-list",
-      },
-    });
+            usage: GPUTextureUsage.RENDER_ATTACHMENT,
+          });
 
-    return () => {
-      console.log(context.canvas.width, context.canvas.height, Date.now());
-      const commandEncoder = device.createCommandEncoder();
+          renderTargetView = renderTarget.createView();
+        }
+        if (renderTargetView) {
+          const commandEncoder = device.createCommandEncoder();
 
-      const textureView = context.getCurrentTexture().createView();
+          const renderPassDescriptor: GPURenderPassDescriptor = {
+            colorAttachments: [
+              {
+                view: renderTargetView,
+                resolveTarget: context.getCurrentTexture().createView(),
+                clearValue: [0.2, 0.2, 0.2, 1.0],
+                loadOp: "clear",
+                storeOp: "store",
+              },
+            ],
+          };
 
-      const renderPassDescriptor: GPURenderPassDescriptor = {
-        colorAttachments: [
-          {
-            view: textureView,
-            clearValue: [0, 0, 0, 1],
-            loadOp: "clear",
-            storeOp: "store",
-          },
-        ],
+          const passEncoder =
+            commandEncoder.beginRenderPass(renderPassDescriptor);
+          passEncoder.setPipeline(pipeline);
+          passEncoder.draw(3);
+          passEncoder.end();
+
+          device.queue.submit([commandEncoder.finish()]);
+        }
       };
-
-      const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-      passEncoder.setPipeline(pipeline);
-      passEncoder.draw(3);
-      passEncoder.end();
-
-      device.queue.submit([commandEncoder.finish()]);
-    };
-  });
+    },
+  );
 
   useEffect(() => {
     Animated.loop(
       Animated.sequence([
         Animated.timing(width.current, {
-          toValue: 20,
+          toValue: window.width,
           duration: 4000,
           useNativeDriver: false,
         }),
         Animated.timing(width.current, {
-          toValue: window.width,
+          toValue: 20,
           duration: 4000,
           useNativeDriver: false,
         }),
