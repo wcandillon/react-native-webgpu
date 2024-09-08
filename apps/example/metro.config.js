fc71c1c5b9018214bad796662667d84ff97f0eff
Saad Najmi <sanajmi@microsoft.com>
2024-09-09 01:54:18 +0200

    [WIP] Attempt fix metro  config

--- apps/example/metro.config.js
+++ apps/example/metro.config.js
@@ -1,11 +1,39 @@
 const { makeMetroConfig } = require("@rnx-kit/metro-config");
-module.exports = makeMetroConfig({
+const path = require('path');
+
+const root = path.resolve(__dirname, '../..');
+const threePackagePath = path.resolve(root, 'node_modules/three');
+
+const extraConfig = {
+  watchFolders: [root],
+  resolver: {
+    extraNodeModules: {
+      'three': threePackagePath,
+    },
+    resolveRequest: (context, moduleName, platform) => {
+      if (moduleName === 'three/webgpu') { 
+        return {
+          filePath: path.resolve(threePackagePath, 'build/three.webgpu.js'),
+          type: 'sourceFile',
+        };
+      }
+      // Let Metro handle other modules
+      return context.resolveRequest(context, moduleName, platform);
+    },
+  },
+
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
-});
+};
+
+const metroConfig = makeMetroConfig(extraConfig);
+metroConfig.resolver.assetExts.push('glb', 'gltf', 'jpg', 'bin', 'hdr');
+
+
+module.exports = metroConfig;
