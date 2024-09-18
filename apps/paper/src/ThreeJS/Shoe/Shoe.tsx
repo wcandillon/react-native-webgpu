15d3f8782b9d644b6b6fcde349d73207bef16082
William Candillon <wcandillon@gmail.com>
2024-09-18 15:23:10 +0200

    :wrench:

--- apps/paper/src/ThreeJS/Shoe/Shoe.tsx
+++ apps/paper/src/ThreeJS/Shoe/Shoe.tsx
@@ -9,17 +9,19 @@ import { useGLTF } from "../assets/AssetManager";
 const Shoe = () => {
   const ref = useRef<THREE.Group>(null!);
   const gltf = useGLTF(require("../assets/jordan/jordan_shoe.glb"));
+  useEffect(() => {
+    if (gltf) {
+      // Center the model
+      const box = new THREE.Box3().setFromObject(gltf.scene);
+      const center = box.getCenter(new THREE.Vector3());
+      gltf.scene.position.x -= center.x;
+      gltf.scene.position.y -= center.y;
+      gltf.scene.position.z -= center.z;
+    }
+  }, [gltf]);
   if (!gltf) {
     return null;
   }
-
-  // Center the model
-  const box = new THREE.Box3().setFromObject(gltf.scene);
-  const center = box.getCenter(new THREE.Vector3());
-  gltf.scene.position.x -= center.x;
-  gltf.scene.position.y -= center.y;
-  gltf.scene.position.z -= center.z;
-
   return (
     <group ref={ref}>
       <primitive object={gltf.scene} />
@@ -33,7 +35,6 @@ export const ShoeDemo = () => {
     <View style={{ flex: 1 }} {...events}>
       <FiberCanvas style={{ flex: 1 }}>
         <color attach="background" args={[0xffffff]} />
-
         <OrbitControls />
         <ambientLight intensity={0.5} />
         <directionalLight intensity={1} position={[5, 10, 7.5]} castShadow />
