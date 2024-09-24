a307bb03cb536659650a7959ccdfdcc3698a3fd1
Henry Allen <hmallen99@gmail.com>
2024-09-24 11:58:59 -0400

    Copy Nitro modules

---
+++ packages/webgpu/cpp/threading/CallInvokerDispatcher.hpp
@@ -0,0 +1,38 @@
+
+//
+// Created by Marc Rousavy on 27.03.24.
+//
+
+#pragma once
+
+#include "Dispatcher.hpp"
+#include <ReactCommon/CallInvoker.h>
+
+namespace margelo::nitro
+{
+
+  using namespace facebook;
+
+  /**
+   * A Dispatcher that uses react::CallInvoker for it's implementation
+   */
+  class CallInvokerDispatcher final : public Dispatcher
+  {
+  public:
+    explicit CallInvokerDispatcher(std::shared_ptr<react::CallInvoker> callInvoker) : _callInvoker(callInvoker) {}
+
+    void runAsync(std::function<void()> &&function) override
+    {
+      _callInvoker->invokeAsync(std::move(function));
+    }
+
+    void runSync(std::function<void()> &&function) override
+    {
+      _callInvoker->invokeSync(std::move(function));
+    }
+
+  private:
+    std::shared_ptr<react::CallInvoker> _callInvoker;
+  };
+
+} // namespace margelo::nitro
---
+++ packages/webgpu/cpp/threading/ThreadPool.hpp
@@ -0,0 +1,55 @@
+//
+//  ThreadPool.hpp
+//  NitroModules
+//
+//  Created by Marc Rousavy on 21.06.24.
+//
+
+#pragma once
+
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <thread>
+#include <vector>
+
+namespace margelo::nitro
+{
+
+  class ThreadPool final
+  {
+  public:
+    /**
+     * Create a new ThreadPool with the given number of fixed workers/threads.
+     */
+    explicit ThreadPool(const char *const name, size_t numThreads);
+    ~ThreadPool();
+
+    /**
+     * Schedules the given task asynchronously on the ThreadPool.
+     * It will run once a worker is available.
+     */
+    void run(std::function<void()> &&task);
+
+  public:
+    /**
+     * Get a static singleton instance - a shared ThreadPool.
+     * The shared ThreadPool has 3 threads.
+     */
+    static std::shared_ptr<ThreadPool> getSharedPool();
+
+  private:
+    std::vector<std::thread> _workers;
+    std::queue<std::function<void()>> _tasks;
+    std::mutex _queueMutex;
+    std::condition_variable _condition;
+    std::atomic<bool> _isAlive;
+    const char *_name;
+    static constexpr auto TAG = "ThreadPool";
+  };
+
+} // namespace margelo::nitro
