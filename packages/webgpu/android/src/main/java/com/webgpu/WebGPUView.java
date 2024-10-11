99015ec3c236a39343969e5de7048bc1a577c048
William Candillon <wcandillon@gmail.com>
2024-10-11 19:30:01 +0200

    :wrench:

--- packages/webgpu/android/src/main/java/com/webgpu/WebGPUView.java
+++ packages/webgpu/android/src/main/java/com/webgpu/WebGPUView.java
@@ -1,23 +1,23 @@
 package com.webgpu;
 
-import androidx.annotation.NonNull;
-
 import android.content.Context;
+import android.graphics.SurfaceTexture;
 import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.view.TextureView;
+import androidx.annotation.NonNull;
 
 import com.facebook.proguard.annotations.DoNotStrip;
 import com.facebook.react.uimanager.ThemedReactContext;
 
-public class WebGPUView extends SurfaceView implements SurfaceHolder.Callback {
+public class WebGPUView extends TextureView implements TextureView.SurfaceTextureListener {
 
   private Integer mContextId;
   private WebGPUModule mModule;
 
   public WebGPUView(Context context) {
     super(context);
-    getHolder().addCallback(this);
+    setSurfaceTextureListener(this);
+    setOpaque(false);
   }
 
   public void setContextId(Integer contextId) {
@@ -36,24 +36,32 @@ public class WebGPUView extends SurfaceView implements SurfaceHolder.Callback {
   }
 
   @Override
-  public void surfaceCreated(@NonNull SurfaceHolder holder) {
-    float density = getResources().getDisplayMetrics().density;
-    float width = getWidth() / density;
-    float height = getHeight() / density;
-    onSurfaceCreate(holder.getSurface(), mContextId, width, height);
-  }
-
-  @Override
-  public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+  public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surfaceTexture, int width, int height) {
     float density = getResources().getDisplayMetrics().density;
     float scaledWidth = width / density;
     float scaledHeight = height / density;
-    onSurfaceChanged(holder.getSurface(), mContextId, scaledWidth, scaledHeight);
+    Surface surface = new Surface(surfaceTexture);
+    onSurfaceCreate(surface, mContextId, scaledWidth, scaledHeight);
   }
 
   @Override
-  public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+  public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surfaceTexture, int width, int height) {
+    float density = getResources().getDisplayMetrics().density;
+    float scaledWidth = width / density;
+    float scaledHeight = height / density;
+    Surface surface = new Surface(surfaceTexture);
+    onSurfaceChanged(surface, mContextId, scaledWidth, scaledHeight);
+  }
+
+  @Override
+  public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surfaceTexture) {
     onSurfaceDestroy(mContextId);
+    return true;
+  }
+
+  @Override
+  public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surfaceTexture) {
+    // This method can be used if you need to handle updates to the texture content.
   }
 
   @DoNotStrip
