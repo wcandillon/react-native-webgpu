c53d6fd9a54481ef57dfccc84a181b21a94e539a
William Candillon <wcandillon@gmail.com>
2024-10-13 21:20:45 +0200

    Remove bogus changes

--- apps/example/src/App.tsx
+++ apps/example/src/App.tsx
@@ -1,7 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { enableScreens } from "react-native-screens";
 
 import type { Routes } from "./Route";
 import { Home } from "./Home";
@@ -34,8 +33,6 @@ import "fast-text-encoding";
 window.parent = window;
 
 const Stack = createNativeStackNavigator<Routes>();
-
-enableScreens(false);
 
 function App() {
   const assets = useAssets();
--- apps/example/src/Home.tsx
+++ apps/example/src/Home.tsx
@@ -27,10 +27,10 @@ export const examples = [
     screen: "Cube",
     title: "🧊 Cube",
   },
-  {
-    screen: "CanvasAPI",
-    title: "🧩 Canvas API",
-  },
+  // {
+  //   screen: "CanvasAPI",
+  //   title: "🧩 Canvas API",
+  // },
   {
     screen: "Cubemap",
     title: "🌉 Cubemap",
