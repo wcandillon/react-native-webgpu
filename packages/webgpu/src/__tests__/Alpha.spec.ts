5e6cbfca7c727bca9efa03fd242c5795331c7758
William Candillon <wcandillon@gmail.com>
2024-10-18 18:35:23 +0200

    Add simple test

---
+++ packages/webgpu/src/__tests__/Alpha.spec.ts
@@ -0,0 +1,28 @@
+import { checkImage, client, encodeImage } from "../setup";
+
+describe("Alpha", () => {
+  it("Premultiplied Color", async () => {
+    const result = await client.eval(({ device, ctx, canvas }) => {
+      const commandEncoder = device.createCommandEncoder();
+      const textureView = ctx.getCurrentTexture().createView();
+      const alpha = 0.5;
+      const renderPassDescriptor: GPURenderPassDescriptor = {
+        colorAttachments: [
+          {
+            view: textureView,
+            clearValue: [0.3 * alpha, 0.6 * alpha, 1 * alpha, alpha],
+            loadOp: "clear",
+            storeOp: "store",
+          },
+        ],
+      };
+
+      const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+      passEncoder.end();
+      device.queue.submit([commandEncoder.finish()]);
+      return canvas.getImageData();
+    });
+    const image = encodeImage(result);
+    checkImage(image, "snapshots/semi-opaque-cyan.png");
+  });
+});
