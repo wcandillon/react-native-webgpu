fec88a3c2c770215f9a16cc2e6dbd2248d679fa7
Scott Ashton <scottmortonashton@gmail.com>
2025-06-05 14:19:11 -0600

    More robust global type augmentation

--- packages/webgpu/src/index.tsx
+++ packages/webgpu/src/index.tsx
@@ -6,4 +6,10 @@ declare global {
   interface Navigator {
     gpu: GPU;
   }
+
+  // If a non-DOM env, this augment global with navigator
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore: Ignore if 'Navigator' doesn't exist
+  // eslint-disable-next-line no-var
+  var navigator: Navigator;
 }
