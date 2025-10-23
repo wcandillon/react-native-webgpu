6cb87e1b99328f0d85e0d316d81e169037d75449
Blazej Kustra <blazej.kustra@swmansion.com>
2025-10-23 18:44:49 +0200

    Implement global methods on web

--- packages/webgpu/src/WebGPUModule/index.web.ts
+++ packages/webgpu/src/WebGPUModule/index.web.ts
@@ -5,3 +5,48 @@ export const WebGPUModule: Spec = {
   getConstants: () => ({}),
   install: () => true,
 };
+
+RNWebGPU.fabric = true;
+RNWebGPU.getNativeSurface = (contextId: number) => {
+  const canvas = document.getElementById(
+    contextIdToId(contextId),
+  ) as HTMLCanvasElement;
+
+  const { height, width } = canvas.getBoundingClientRect()!;
+
+  return {
+    surface: BigInt(contextId),
+    height,
+    width,
+    clientHeight: height,
+    clientWidth: width,
+  };
+};
+
+function contextIdToId(contextId: number) {
+  return "rnwgpu-canvas-" + contextId;
+}
+
+RNWebGPU.MakeWebGPUCanvasContext = (
+  contextId: number,
+  width: number,
+  height: number,
+) => {
+  const canvas = document.getElementById(
+    contextIdToId(contextId),
+  ) as HTMLCanvasElement;
+
+  if (
+    canvas.getAttribute("width") !== width.toString() ||
+    canvas.getAttribute("height") !== height.toString()
+  ) {
+    canvas.setAttribute("width", width.toString());
+    canvas.setAttribute("height", height.toString());
+  }
+
+  const context = canvas.getContext("webgpu")!;
+
+  return Object.assign(context, {
+    present: () => {},
+  });
+};
